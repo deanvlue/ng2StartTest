b8a38c73c2d91013cf76c92e24fe84309de8d57d
Carlos Munoz <carlos.munoz@alsea.com.mx>
2016-06-07 13:51:22 -0500

    cleanup code

--- vipsisaalertasoporte/index.js
+++ vipsisaalertasoporte/index.js
@@ -6,24 +6,8 @@ var Request = require('tedious').Request;
 var moment = require('moment');
 
 var sendgrid = require('sendgrid')('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');
-//api key=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
 
 var soporte = require('./soporte.json');
-
-/*var sp = 'SPInformacionEncuestas';
-var paAccion='TE';
-var paInError = 0;
-var paVcError = 0;
-var paIdMarca = 1;
-var paIncluyeTienda = 1;*/
-
-
-/*var config = {
-    userName: 'reportesti',
-    password: 'Alsea.15',
-    server: 'w2z7yuuxg2.database.windows.net',
-    options: {encrypt:true, database:'alsea-isa-prod'}
-};*/
 
 var config_dw = {
       userName: 'starbucks_so',
@@ -45,7 +29,7 @@ var errores = [];
 
 
 function mandaCorreo(store){
-  //console.log(store);
+
     var correo = "vips"+store.fiidtienda+"@vips.com.mx";
     var cc_soporte=[];
     
@@ -62,16 +46,10 @@ function mandaCorreo(store){
        errores.push(store.fiidtienda);
      }
     
-    // TODO: Validar la diferencia de días
     if(moment(store.fdultimoenvio).isValid()){
         var fechaUltimoEnvio = moment(store.fdultimoenvio);
         
         if(fechaUltimoEnvio.diff(ahora, 'days') < -2){
-            //var email = new sendgrid.Email();
-            
-            //var copiados = 'carlos.munoz@alsea.com.mx; elias.carrillo@alsea.com.mx';
-            //correo+="; ";
-            //correo+=copiados;
             
             var email     = new sendgrid.Email({
               to:       correo,
@@ -85,16 +63,13 @@ function mandaCorreo(store){
 
             console.log("Armando Correo...");
             
-           /* sendgrid.send(email, function(err, json){
+            sendgrid.send(email, function(err, json){
               if (err) { return console.error(err); }
               console.log(json);
-            });*/
-            console.log(email);
+            });
+            //console.log(email);
             
             console.log("Enviando Correo a: " + correo);
-       
-            //console.log("Mandando Correo a tienda: "+ store.FIIdTienda + " " + store.FCNombre);
-            //console.log(" Unidad favor de levantar su ticket a " + correo);
         }
     }
 }
@@ -104,18 +79,6 @@ connection.on('connect', function(err){
   if(err){
     return console.log("Hubo un error: "+ err);
   }
-
-    
-   /* var request = new Request(sp, function(err){
-    if(err)
-        console.log("Error en el request: "+err); 
-    });
-    
-    request.addParameter('paAccion', TYPES.VarChar,  paAccion);
-    request.addParameter('paInError', TYPES.Int,  paInError);
-    request.addParameter('paVcError', TYPES.Int,  paVcError);
-    request.addParameter('paIdMarca', TYPES.Int,  paIdMarca);
-    request.addParameter('paIncluyeTienda', TYPES.Int,  paIncluyeTienda);*/
     
    if(err){
       console.log(err);
@@ -132,17 +95,11 @@ connection.on('connect', function(err){
     });
 
     spRequest.on('row', function(columns){
-      //console.log(columns);
        columns.forEach(function(column){
   
-        
         if(column.metadata.colName==='fiidtienda' || column.metadata.colName==='fcnombre'|| column.metadata.colName==='fdultimoenvio'){
-             //console.log(column.metadata.colName + '|' + column.value);
              tienda[column.metadata.colName]=column.value;
-             
-           
          } 
-        //console.log(column.metadata.colName + '|' + column.value);
        });
       tiendas.push(tienda);
       tienda = {};
@@ -163,31 +120,10 @@ connection.on('connect', function(err){
        
        console.log("Obteniendo Datos" );
        console.log(errores);
-       //mandaUnCorreo(tiendas);
-        
+       
     });
-    
-    //connection.callProcedure(request);
+
     connection.execSql(spRequest);
     
 });
 
-/*
-function mandaUnCorreo(stores){
-     var email = new sendgrid.Email();
-     var destinatarios = ['carlos.munoz@alsea.com.mx','elias.carrillo@alsea.com.mx'];
-     //email.addTo('carlos.munoz@alsea.com.mx');
-     //email.addTo('elias.carrillo@alsea.com.mx');
-     //email.addTo(destinatarios);
-     email.addCc('elias.carrillo@alsea.com.mx');
-     email.addTo('carlos.munoz@alsea.com.mx');
-     email.setFrom("noreply@vips.com.mx");
-     email.setSubject("Prueba de Envio desde SendGrid");
-     
-     var contenido = JSON.stringify(stores);
-     
-     email.setHtml("<h3>Prueba</h3><br><p> Favor de enviar un correo a soporte ya que su tienda no ha enviado encuestas en más de dos días</p>" + contenido);
-     console.log("Armando Correo...");
-     //sendgrid.send(email);
-     console.log("Enviando Correo");
-}*/
